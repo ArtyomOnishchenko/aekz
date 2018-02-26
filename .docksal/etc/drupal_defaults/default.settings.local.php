d88738868284fd2bebb50f0db376f8d368165423
Melnik Dmitry <dima@avis.codes>
2018-02-26 21:52:13 +0200

    Add setup command.

---
+++ .docksal/etc/drupal_defaults/default.settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = array (
+    'database' => 'default',
+    'username' => 'user',
+    'password' => 'user',
+    'prefix' => '',
+    'host' => 'db',
+    'port' => '3306',
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
+
+$config_directories['sync'] = '../config/sync';
+$settings['hash_salt'] = 'NcLZvJhWZTnWTyRDUwYU6bQmuyVAw_U01Pz1VqQur7v9iYBBj9Pja5N6guN-Lnw7ZHpAZzSoiQ';
